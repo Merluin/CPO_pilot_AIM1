0a1815c00893612c8d2f8de3700723d1c117e906
thomas QUETTIER <thomasquettier@MBP-di-thomas.lan>
2022-03-17 19:30:42 +0100

    new scripts

---
+++ 06.scipts/data_exploration.R
@@ -0,0 +1,40 @@
+#################################################
+# 
+# Name:           data exploration
+# Programmer:     Thomas Quettier
+# Date:           18/11/2021
+# Description:    load dataset
+#
+#################################################
+
+
+
+# Library & dataset
+rm(list=ls()) # remove all objects
+# neaded packages
+library(dplyr)
+library(tidyverse)
+
+# loading data ----
+load("data/dataset.RData") 
+
+
+dataset
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################################################
+# 
+# END
+#
+#################################################
