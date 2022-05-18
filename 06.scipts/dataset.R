648f2172c04dd0f7a2935404bd60b14022009895
thomas QUETTIER <thomasquettier@MBP-di-thomas.lan>
2022-05-18 10:50:58 +0200

    NF

--- 06.scipts/dataset.R
+++ 06.scipts/dataset.R
@@ -15,20 +15,22 @@ library(dplyr)
 library(tidyverse)
 
 # find nb participant
-folder_dir<-c("03.original_data/")
+#folder_dir<-c("03.original_data/Pilot/") # use ","
+folder_dir<-c("03.original_data/OFMT/")   # use ";"
 foldername<-list.files(folder_dir,pattern='csv')
 nbsub<-sapply(folder_dir,function(folder_dir){length(foldername)})
 effective<-c(1:nbsub)
 
-# loading data ----
-dataset <- read.csv(paste(folder_dir,foldername[1],sep = ""), sep=",", header=TRUE,stringsAsFactors = FALSE)
+# loading data ---- !!!!!!!! "," or  ";"
+dataset <- read.csv(paste(folder_dir,foldername[1],sep = ""), sep=";", header=TRUE,stringsAsFactors = FALSE)
 
 for (i in 2:length(effective)){
-  data2<-read.csv(paste(folder_dir,foldername[i],sep = ""), sep=",", header=TRUE,stringsAsFactors = FALSE)
+  data2<-read.csv(paste(folder_dir,foldername[i],sep = ""), sep=";", header=TRUE,stringsAsFactors = FALSE)
   dataset<-rbind(dataset,data2)
 }
 
-save(dataset,file="04.data_preprocessing/dataset.RData")
+#save(dataset,file="04.data/dataset_pilot.RData")
+save(dataset,file="04.data/dataset_OFMT.RData")
 
 #################################################
 # 
